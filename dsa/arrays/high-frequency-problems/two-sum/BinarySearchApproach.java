99b5d1147b3b25be5526a38d884cdc2091f725d2
Swar Künwar <64254264+swar-101@users.noreply.github.com>
2025-01-25 17:31:24 +0530

    Add Binary Search Approach

--- dsa/arrays/high-frequency-problems/two-sum/BinarySearchApproach.java
+++ dsa/arrays/high-frequency-problems/two-sum/BinarySearchApproach.java
@@ -3,16 +3,18 @@ import java.util.Arrays;
 public class BinarySearchApproach {
     // NOTE:
 // This approach works only if the array is already sorted.
-// Two-pointer technique:
-// Start with one pointer at the beginning (left) and the other at the end (right) of the array.
-// If the sum of the two elements is greater than the target, move the right pointer leftward to reduce the sum.
-// If the sum is less than the target, move the left pointer rightward to increase the sum.
+// Using Binary Search:
+//  1. Iterate through the array using a loop.
+//	2. For each element, calculate its complement.
+//	3. Perform Binary Search for this complement in the remaining array.
+//	4. If the complement is found, return the indices of the current element and the complement.
+//	5. If no pair is found return 'null'.
 
     // a = [1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 10]
     // target = 19
 
     public static int[] twoSumPair(int[] a, int target) {
-        if ( Arrays.equals(a, null) || Arrays.equals(a, new int[0])) {
+        if (Arrays.equals(a, null) || Arrays.equals(a, new int[0])) {
             return null;
         }
 
