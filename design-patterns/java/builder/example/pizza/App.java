8603d8e5364300a858ef2fa3c9bc128b0c2655ff
Swar Künwar <64254264+swar-101@users.noreply.github.com>
2025-02-19 10:53:04 +0530

    Add Design Pattern: Builder Pattern

---
+++ design-patterns/java/builder/example/pizza/App.java
@@ -0,0 +1,13 @@
+package pizza;
+
+public class App {
+    public static void main(String[] args) {
+        Pizza pizza = new Pizza.Builder()
+                .setDough("Thin Crust")
+                .setSauce("Tomato Basil")
+                .setCheese("Mozzarella")
+                .setTopping("Pepperoni").build();
+        
+        System.out.println(pizza.toString());
+    }
+}
---
+++ design-patterns/java/builder/example/pizza/Pizza.java
@@ -0,0 +1,52 @@
+package pizza;
+
+public class Pizza {
+    private String dough;
+    private String sauce;
+    private String cheese;
+    private String topping;
+
+    private Pizza(Builder builder) {
+        this.dough = builder.dough;
+        this.sauce = builder.sauce;
+        this.cheese = builder.cheese;
+        this.topping = builder.topping;
+    }
+
+    public static class Builder {
+        private String dough;
+        private String sauce;
+        private String cheese;
+        private String topping;
+
+        public Builder setDough(String dough) {
+            this.dough = dough;
+            return this;
+        }
+
+        public Builder setSauce(String sauce) {
+            this.sauce = sauce;
+            return this;
+        }
+
+        public Builder setCheese(String cheese) {
+            this.cheese = cheese;
+            return this;
+        }
+
+        public Builder setTopping(String topping) {
+            this.topping = topping;
+            return this;
+        }
+
+        public Pizza build() {
+            return new Pizza(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "A hot Pizza with " + dough + " and " + sauce + " sauce, " + cheese + " cheese and "
+                + topping + " toppings!";
+    }
+}
